87d32a3948bf485ba5052d9ad26e3e4a23065673
Uítalo Souza <uitalo.urss@gmail.com>
2024-12-13 13:17:47 -0300

    implement field validator in request body

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 class Course(BaseModel):
     id: Optional[int] = None
@@ -7,3 +7,25 @@ class Course(BaseModel):
     lesson: int
     hour: int
 
+    @field_validator('title')
+    def validate_title(cls, value):
+        title_to_validate = value.split(" ")
+
+        if len(title_to_validate) < 3:
+            raise ValueError("Nome do título deve ter pelo menos três palavras")
+        
+        return value
+    
+    @field_validator('lesson')
+    def validate_lesson(cls, value):
+        if value < 10:
+            raise ValueError("Quantidade de aulas deve ser maior que 10.")
+        
+        return value
+    
+    @field_validator('hour')
+    def validate_hour(cls, value):
+        if value < 32:
+            raise ValueError("Carga horária deve ser maior que 32 horas.")
+        
+        return value
