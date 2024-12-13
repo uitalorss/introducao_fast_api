f256b3255af33a3782817dd5be75b5eebe89146a
Uítalo Souza <uitalo.urss@gmail.com>
2024-12-13 10:06:44 -0300

    implement endpoint to delete a course

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-from fastapi import FastAPI, HTTPException, Response, status
+from typing import Any
+from fastapi import FastAPI, HTTPException, Response, Depends, status
 from fastapi.responses import JSONResponse
 from cursos import courses
 from models import Course
+from time import sleep
 
-app = FastAPI()
 
-@app.get("/")
+app = FastAPI(
+    title='Introdução ao FastAPI',
+    description='API de teste para estudo do FastAPI',
+    version='0.0.1')
+
+def fake_db():
+    try:
+        print("Abrindo conexão com banco de dados.")
+        sleep(2)
+    finally:
+        print("fechando conexão com banco de dados.")
+        sleep(2)
+
+@app.get("/", tags=['intro'])
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item}")
+@app.get("/items/{item}", tags=['courses'])
 async def get_item(item: str):
     return {"item": item}
 
-@app.get("/cursos")
-async def get_courses():
+@app.get("/cursos", tags=['courses'])
+async def get_courses(db: Any = Depends(fake_db)):
     return courses
 
-@app.get("/cursos/{id}")
+@app.get("/cursos/{id}", tags=['courses'])
 async def get_course(id: int):
     course = [item for item in courses if item["id"] == id]
     if course:
@@ -26,7 +40,7 @@ async def get_course(id: int):
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Usuário não encontrado.')
     
-@app.post("/cursos", status_code=status.HTTP_201_CREATED)
+@app.post("/cursos", tags=['courses'], status_code=status.HTTP_201_CREATED)
 async def create_course(course: Course):
     course_in_list = [item for item in courses if item["id"] == course.id]
     if not course_in_list:
@@ -35,7 +49,7 @@ async def create_course(course: Course):
     else:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Problema com relação a id do curso.")
     
-@app.put("/cursos/{course_id}")
+@app.put("/cursos/{course_id}", tags=['courses'])
 async def update_course(course_id: int, course: Course):
     for item in courses:
         if item["id"] == course_id:
@@ -46,12 +60,10 @@ async def update_course(course_id: int, course: Course):
 
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Curso não encontrado.")
     
-@app.delete("/cursos/{course_id}")
+@app.delete("/cursos/{course_id}", tags=['courses'])
 async def delete_course(course_id: int):
-    item_to_remove = {}
-    for item in courses:
-        if(item["id"] == course_id):
-            item_to_remove = item
+    item_to_remove = next((item for item in courses if item["id"] == course_id), None)
+    if not item_to_remove:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado.')
     
-    courses.remove(item_to_remove)
-    return Response(status_code=status.HTTP_201_CREATED)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
