4e5db31f97e54c9575cdb0e25a8ac5397d4eebfd
Uítalo Souza <uitalo.urss@gmail.com>
2024-12-16 11:33:07 -0300

    ajuste para implementar o dotenv no projeto

--- core/config.py
+++ core/config.py
@@ -2,13 +2,23 @@ from typing import ClassVar
 from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 import os
+import dotenv
+
+dotenv.load_dotenv()
 
 class Settings(BaseSettings):
+    DB_NAME: ClassVar[str] = os.getenv("DB_NAME")
+    DB_HOST: ClassVar[str] = os.getenv("DB_HOST")
+    DB_PORT: ClassVar[str] = os.getenv("DB_PORT")
+    DB_USER: ClassVar[str] = os.getenv("DB_USER")
+    DB_PASS: ClassVar[str] = os.getenv("DB_PASS")
+    DB_BANCO: str = "postgresql+asyncpg"
+
     API_V1_STR: ClassVar[str] = "/api/v1"
-    DB_URL: ClassVar[str] = 'postgresql+asyncpg://postgres:postgres@localhost:5432/faculdade'
+    DB_URL: ClassVar[str] = f"{DB_BANCO}://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}" #'postgresql+asyncpg://postgres:postgres@localhost:5432/faculdade'
     DBBaseModel: ClassVar[DeclarativeMeta] = declarative_base()
 
-    JWT_KEY: str = "XeybrOiX9uGQmjkriHzSIABiD6woVmGSqCni8l5Pc_Y"
+    JWT_KEY: str = os.getenv("JWT_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 8
 
